84e052e2bc55995d37c26f086880cc68205ce89b
nzommmo <ericnzomo17@gmail.com>
2024-02-08 02:02:45 +0300

    Bus Routes

--- BusRoutes/BusRoutes.js
+++ BusRoutes/BusRoutes.js
@@ -1,39 +1,17 @@
-
-/*
-function search(){
-    let stationsearch = document.getElementById("stationsearch")
-
-var filter = stationsearch.value.toLowerCase()
-var dropdown = document.getElementById("station")
-var options = dropdown.getElementsByTagName("option")
-for (var i = 0;i < options.length; i++ ){
-    var optiontext = options[i].textContent.toLowerCase()
-    if(optiontext.indexOf(filter)>-1) {
-        options[i].style.display =''
-    }else{
-        options[i].style.display ="none"
-    }
- }
- dropdown.classList.add("show")
-
-
-}
-
-stationsearch.addEventListener("input",search)
-*/
-
  let keywords= [
     "CUEA",
     "Bomas",
     "Rongai",
     "Maasai Lodge",
-    "Tumaini"
+    "Tumaini",
+    "congo",
+    "Rogo"
 
  ];
 
  const resultbox = document.querySelector(".resultbox");
  const inputbox = document.getElementById("inputbox")
-
+const From = document.getElementById("From")
  inputbox.onkeyup = function(){
     let result = []
     let input = inputbox.value
@@ -41,44 +19,31 @@ stationsearch.addEventListener("input",search)
         result = keywords.filter((keyword)=>{
            return keyword.toLowerCase().includes(input.toLowerCase())
         })
-        console.log(result)
+      
     }
     display(result)
     if(!result.length){
         resultbox.innerHTML=""
     }
+
+   
+
+   
+   
  }
  function display(result){
     const content = result.map((list)=>{
         return "<li onclick=selectInput(this)>" + list + "</li>"
     })
+
+    
     resultbox.innerHTML = "<ul>" + content.join('') + "</ul>"
  }
+
 
 function selectInput(list){
     inputbox.value = list.innerHTML
     resultbox.innerHTML =""
+    From.innerHTML = list.innerHTML
+    
 }
-const Routenumber  = document.getElementById("Routenumber")
-const searchinput =  document.getElementById("search")
-const route = Routenumber.value
-
-function checkinput(){
-    if (Routenumber.value == 125){
-        searchinput.value ="Rongai"        
-    }else if(Routenumber.value == 48) {
-        searchinput.value == "CUEA"
-    }
-    else{
-        searchinput.value =""
-    }
-
-}
-checkinput()
-
-function clear(){
-    searchinput.value = ""
-    Routenumber.value = ""
-}
-
-//addEventListener("click",clear)
