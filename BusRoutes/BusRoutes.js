7c3a1d797ebd31f73cb25f5e6c55666d8ca6b1d2
nzommmo <ericnzomo17@gmail.com>
2024-02-08 18:03:01 +0300

    update

--- BusRoutes/BusRoutes.js
+++ BusRoutes/BusRoutes.js
@@ -1,4 +1,4 @@
- let keywords= [
+let keywords = [
     "CUEA",
     "Bomas",
     "Rongai",
@@ -6,44 +6,91 @@
     "Tumaini",
     "congo",
     "Rogo"
+];
 
- ];
+const resultboxes = document.querySelectorAll(".resultbox");
+const inputboxes = document.querySelectorAll(".input");
+const routesSelect = document.getElementById("Routenumber");
+const boardingDiv = document.getElementById("BusFrom");
+const alightDiv = document.getElementById("BusTo");
+const plate = document.getElementById("plateno")
+const fareamount  = document.getElementById("fare")
 
- const resultbox = document.querySelector(".resultbox");
- const inputbox = document.getElementById("inputbox")
-const From = document.getElementById("From")
- inputbox.onkeyup = function(){
-    let result = []
-    let input = inputbox.value
-    if (input.length){
-        result = keywords.filter((keyword)=>{
-           return keyword.toLowerCase().includes(input.toLowerCase())
-        })
-      
-    }
-    display(result)
-    if(!result.length){
-        resultbox.innerHTML=""
-    }
+inputboxes.forEach((inputbox, index) => {
+    inputbox.addEventListener('keyup', function () {
+        let result = [];
+        let input = inputbox.value;
 
-   
+        if (input.length) {
+            result = keywords.filter((keyword) => {
+                return keyword.toLowerCase().includes(input.toLowerCase());
+            });
+        }
 
-   
-   
- }
- function display(result){
-    const content = result.map((list)=>{
-        return "<li onclick=selectInput(this)>" + list + "</li>"
-    })
+        display(result, index);
+        if (!result.length) {
+            resultboxes[index].innerHTML = "";
+        }
+    });
+});
 
+routesSelect.addEventListener('change', function() {
+    let selectedValue = routesSelect.value;
     
-    resultbox.innerHTML = "<ul>" + content.join('') + "</ul>"
- }
+    if (selectedValue== 48) {
+        inputboxes[0].value = "Rongai";
+        boardingDiv.innerHTML = "Boarding: " + inputboxes[0].value;
+        resultboxes[0].innerHTML = "";
+        From.innerHTML = inputboxes[0].value;
+        plate.innerHTML = "KAY 485T"
+        let rate = 20;
+
+    }else if(selectedValue== 125){
+        inputboxes[0].value = "Ngong";
+        boardingDiv.innerHTML = "Boarding: " + inputboxes[0].value;
+        resultboxes[0].innerHTML = "";
+        From.innerHTML = inputboxes[0].value;
+        plate.innerHTML = "KBZ 325H"
+        let rate = 20;
+    }
+        
 
 
-function selectInput(list){
-    inputbox.value = list.innerHTML
-    resultbox.innerHTML =""
-    From.innerHTML = list.innerHTML
+
+            function calcfare(){
+                
+                fare = 0.5 * rate
+                fareamount.innerHTML = fare
+
+            }
+            calcfare()
+            
+        
     
+});
+
+function display(result, index) {
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this," + index + ")>" + list + "</li>";
+    });
+
+    resultboxes[index].innerHTML = "<ul>" + content.join('') + "</ul>";
 }
+
+function selectInput(list, index) {
+    inputboxes[index].value = list.innerHTML;
+    resultboxes[index].innerHTML = "";
+    if (index === 0) {
+        From.innerHTML = list.innerHTML;
+        boardingDiv.innerHTML = "Boarding: " + list.innerHTML;
+        if (list.innerHTML.toLowerCase() === "rongai") {
+            // Display "Welcome" when "Rongai" is selected
+            console.log("Welcome");
+        }
+    } else if (index === 1) {
+        TO.innerHTML = list.innerHTML;
+        alightDiv.innerHTML = "Alight: " + list.innerHTML;
+    }
+}
+
+
---
+++ BusRoutes/Destinaton.js
@@ -0,0 +1,49 @@
+let keywords= [
+    "CUEA",
+    "Bomas",
+    "Rongai",
+    "Maasai Lodge",
+    "Tumaini",
+    "congo",
+    "Rogo"
+
+ ];
+
+ const resultbox = document.querySelector(".resultbox1");
+ const inputbox = document.getElementById("inputbox1")
+const From = document.getElementById("From")
+ inputbox.onkeyup = function(){
+    let result = []
+    let input = inputbox.value
+    if (input.length){
+        result = keywords.filter((keyword)=>{
+           return keyword.toLowerCase().includes(input.toLowerCase())
+        })
+      
+    }
+    display(result)
+    if(!result.length){
+        resultbox1.innerHTML=""
+    }
+
+   
+
+   
+   
+ }
+ function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick=selectInput(this)>" + list + "</li>"
+    })
+
+    
+    resultbox.innerHTML = "<ul>" + content.join('') + "</ul>"
+ }
+
+
+function selectInput(list){
+    inputbox.value = list.innerHTML
+    resultbox.innerHTML =""
+    From.innerHTML = list.innerHTML
+    
+}
